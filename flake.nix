81f146caaf9267e8d0368af7ac1af3e774070acf
loockeeer <lulu.r1313@gmail.com>
2026-02-02 16:15:49 +0100

    chore: use OCaml + Flambda

--- flake.nix
+++ flake.nix
@@ -11,40 +11,32 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { system = system; };
-      libs = with pkgs; [
+      ocaml = pkgs.ocaml.override { flambdaSupport = true; };
+      opkgs = pkgs.ocaml-ng.mkOcamlPackages ocaml;
+      libs = [
         ocaml
-        dune_3
-        ocamlPackages.findlib
-        ocamlformat
+        pkgs.dune_3
+        opkgs.findlib
+        pkgs.ocamlformat
 
         # GFX_SDL module's libraries
-        ocamlPackages.tsdl
-        ocamlPackages.tsdl-image
-        ocamlPackages.tsdl-ttf
+        opkgs.tsdl
+        opkgs.tsdl-image
+        opkgs.tsdl-ttf
 
         # GFX_JSOO module's libraries
-        ocamlPackages.js_of_ocaml
-        ocamlPackages.js_of_ocaml-ppx
+        opkgs.js_of_ocaml
+        opkgs.js_of_ocaml-ppx
       ];
-      ppfa = pkgs.ocamlPackages.buildDunePackage {
-        pname = "ppfa";
-        version = "1.0.0";
-
-        minimalOcamlVersion = "5.2.1";
-
-        src = ./.;
-      };
     in
     {
       formatter.${system} = nixpkgs.legacyPackages.${system}.nixfmt-tree;
       devShells.${system}.default = pkgs.mkShell {
-        packages = libs ++ [pkgs.simple-http-server];
+        packages = libs ++ [ pkgs.simple-http-server ];
         shellHook = ''
-            simple-http-server -i -p 2131 . &> /dev/null
-            echo "Static server at http://localhost:2131 !"
+          simple-http-server -i -p 2131 . &> /dev/null &
+          echo "Static server at http://localhost:2131 !"
         '';
       };
-
-      packages.${system}.default = ppfa;
     };
 }
