59874295f1ddf8378ab82ba09d7f2c5761cb4a92
loockeeer <lulu.r1313@gmail.com>
2026-02-16 22:32:35 +0100

    fix: switch web keys

--- prog/game_js.ml
+++ prog/game_js.ml
@@ -1,4 +1,4 @@
 (* Main spécifique à Javascript *)
 let debug = Gfx.open_formatter "console"
 let () = Gfx.set_debug_formatter debug
-let () = Game.run Game.{ move_left = "d"; move_right = "q"; jump = " " }
+let () = Game.run Game.{ move_left = "q"; move_right = "d"; jump = " " }
