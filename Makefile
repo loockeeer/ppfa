5f14316cf162884727458ad8176482b3a523329e
loockeeer <lulu.r1313@gmail.com>
2026-04-18 15:53:24 +0200

    chore: add rapport, asciidoc deps and build system

---
+++ Makefile
@@ -0,0 +1,11 @@
+build:
+	dune build
+
+build_sdl:
+	dune build @sdl
+
+run_sdl:
+	dune exec prog/game_sdl.exe
+
+doc:
+	asciidoctor-pdf rapport/main.adoc
--- flake.nix
+++ flake.nix
@@ -31,6 +31,9 @@
         # Development
         opkgs.utop
         opkgs.ocaml-lsp
+
+		# Rapport
+		pkgs.asciidoctor
       ];
     in
     {
@@ -38,7 +41,7 @@
       devShells.${system}.default = pkgs.mkShell {
         packages = libs ++ [ pkgs.simple-http-server ];
         shellHook = ''
-          simple-http-server -i -p 2131 . &> /dev/null &
+		  simple-http-server -i -p 2131 . &> /dev/null &
           echo "Static server at http://localhost:2131 !"
         '';
       };
